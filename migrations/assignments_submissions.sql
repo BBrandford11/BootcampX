79fc5666b677441f4191a82125292b3f2cda2152
BBrandford11 <brandfordbryce@gmail.com>
2022-03-08 14:10:56 -0700

    adds new tables and info

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -14,3 +14,12 @@ CREATE TABLE assignment_submissions (
   duration INTEGER,
   submission_date DATE
 );
+
+CREATE TABLE teacher (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  is_active ,
+
+);
